53fe2350b4bba2641fe664c33ebbda8522851b01
Aayan Aqdas <80333428+aayanaqdas@users.noreply.github.com>
2025-03-01 00:50:54 +0100

    todolist app first design and add to list function

---
+++ src/app/todo/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+import { Todo } from "@/components/todoList";
+
+export default function TodoPage() {
+  return (
+    <div className=" p-4 min-h-screen bg-gray-900">
+      <Todo />
+    </div>
+  );
+}
---
+++ src/components/todoInputs.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState } from "react";
+
+export function TodoInputs({ addTask }) {
+  const [taskName, setTaskName] = useState("");
+  const [taskDesc, setTaskDesc] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (taskName.trim() && taskDesc.trim()) {
+      addTask({ name: taskName, description: taskDesc });
+      setTaskName("");
+      setTaskDesc("");
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-6 items-center justify-center h-full w-full "
+    >
+      <h1 className=" mb-2 mt-2 text-3xl text-white">Add task</h1>
+      <div className="flex flex-col gap-4 w-full max-w-sm">
+        <input
+          className="px-5 py-4 outline-0 bg-gray-800 placeholder-gray-400 text-sm text-gray-400 w-full shadow-xl "
+          type="text"
+          placeholder="Write taskname"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+        />
+        <input
+          className="px-5 py-4 outline-0  bg-gray-800 placeholder-gray-400 text-sm text-gray-400 w-full shadow-xl "
+          type="text"
+          placeholder="Write task description"
+          value={taskDesc}
+          onChange={(e) => setTaskDesc(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="py-2 mt-4 cursor-pointer rounded-sm bg-blue-600 text-white text-center w-full"
+        >
+          Add to list
+        </button>
+      </div>
+    </form>
+  );
+}
